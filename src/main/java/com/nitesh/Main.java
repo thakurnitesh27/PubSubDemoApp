3f68758509518c6f015c0f2aa475baba5ea9a5a7
thakurnitesh27 <thakurnitesh27@gmail.com>
2018-11-18 22:11:15 +0530

    Initial commit

---
+++ src/main/java/com/nitesh/Main.java
@@ -0,0 +1,21 @@
+package com.nitesh;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.ProjectTopicName;
+
+import java.util.List;
+
+public class Main {
+
+    private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
